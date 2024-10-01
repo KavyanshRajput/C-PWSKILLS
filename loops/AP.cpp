e31d442fbb3fbea07271b483712d635612f9d334
KavyanshRajput <kavyanshsinghrajput@gmail.com>
2024-10-01 13:41:31 +0530

    Initial commit

---
+++ loops/AP.cpp
@@ -0,0 +1,43 @@
+//Daisplay this AP - 1,3,5,7,9....upto 'n' terms.
+//in mathematics 
+//we know a=1 , d=2
+//an= a+(n-1)d
+//1+(n-1)2
+// //2n-1
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,i;
+// cout<<"Enter the value of n: \n";
+// cin>>n;
+// //......BY ARTHEMATIC PROGRESSION.....
+// // for(i=1;i<=(2*n)-1;i=i+2){
+// //     cout<<i<<endl;
+// // }
+// //.........WITHOUT AP...............
+// //seperate variable
+// //AP-->4,7,10,13....
+// int a = 4;
+// for(i=1;i<=n;i++){
+//     cout<<a<<endl;
+//     a=a+3;
+// }
+// }
+
+//DISPLAY THID GP--> 1,2,4,8,16.......
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int n,a=1,i;
+// cout<<"Enter value of n: ";
+// cin>>n;
+
+//  for(i=1;i<=n;i++){
+//     cout<<a<<endl;
+//     a= a * 2;}
+//     return 0;
+// }
+
+
+
+
